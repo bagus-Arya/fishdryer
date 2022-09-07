3b3dc654f6f08be7c0f8f80ca8f704aa7d35b55c
agum aditya <agumaditya71@gmail.com>
2022-09-08 01:26:31 +0800

    Update BypassTls.php

--- app/Http/Controllers/BypassTls.php
+++ app/Http/Controllers/BypassTls.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Http;
 
 use Illuminate\Http\Request;
 
 class BypassTls extends Controller
 {
     public function index(Request $request){
-        
+        $validate = $request->validate([
+            'url'=>'required|url',
+            'dump'=>'required',
+        ]);
+        $response = Http::acceptJson()->post($validate["url"], $validate["dump"])->json();
+        return $response;
     }
 }
